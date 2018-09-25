7a5ce17c323bd1e3d54bb166c85322396d211be4
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-25 21:56:00 +0300

    test

---
+++ src/views/Child.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Message from Parent: {{ dataName }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    dataName: {
+      type: String,
+      required: true,
+      // default: "Hi",
+    }
+  }
+};
+</script>
+
+
+
+
+
